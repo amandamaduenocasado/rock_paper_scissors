2e2d410efd9fc095ab5da2cf7ab2ff5d3b901540
amandamaduenocasado <amandacaso97@gmail.com>
2024-11-11 09:43:23 +0100

    update

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+/* INSTRUCCIONES
+
+- Localizar los elementos implicados del DOM
+- Crear los datos del programa necesarios
+
+FLUJO DEL PROGRAMA
+ 
+- Detectar dónde hacemos click (Si tenéis problemas al hacer click porque detectáis cosas que no os interesan, podéis usar la propiedad "pointer-events:none" en CSS para facilitaros la vida)
+- Guardar nuestra jugada
+- Generar una jugada aleatoria para el ordenador y guardarla
+- Comparar jugadas
+- Mostrar resultado
+- Asignar puntos 
+
+SE NECESITA 
+
+Pagina principal 
+- Pagina simple 
+    - Primer contenedor fijo/contador
+        Dos contadores 
+            Dos variables que guardan los puntos, en las que cambiará el textcontent
+        Texto
+    - Segundo contenedor/play "game-items"
+        Tres botones 
+    - Tercer contenedor/result "game-results"
+        Botón play again
+    - Extra contenedor/ventana modal 
+
+- Pagina advanced
+- */
+
